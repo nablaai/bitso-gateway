c48f9ce6cf2195eb1792b2b057c0b06eaf02208b
rhdzmota <rohdzmota@gmail.com>
2018-03-03 14:20:43 -0600

    Akka Streams and Kafka implementation
    Basic implementation of Akka Streams and Kafka Producer.

--- bitso-gateway/src/main/scala/com/nablaai/crypto/gateway/Main.scala
+++ bitso-gateway/src/main/scala/com/nablaai/crypto/gateway/Main.scala
@@ -1,33 +1,56 @@
 package com.nablaai.crypto.gateway
 
+import akka.Done
 import akka.http.scaladsl.Http
 import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
 import akka.http.scaladsl.model.ws.{Message, TextMessage, WebSocketRequest}
 
-import scala.concurrent.Promise
+import com.nablaai.util.kafka.{KafkaService, KafkaServiceConfig}
 
+import scala.concurrent.{Future, Promise}
 
 object Main extends Context {
 
   def main(args: Array[String]): Unit = {
 
-    val flow: Flow[Message, Message, Promise[Option[Message]]] =
-    Flow.fromSinkAndSourceMat(
-      Sink.foreach[Message](println),
-      Source(List(
-        TextMessage(Settings.Bitso.subscription_trades),
-        TextMessage(Settings.Bitso.subscription_order),
-        TextMessage(Settings.Bitso.subscription_diff_orders)))
-        .concatMat(Source.maybe[Message])(Keep.right))(Keep.right)
+    val kafkaServiceConfig: KafkaServiceConfig =
+      KafkaServiceConfig(Settings.Kafka.boostrapServer, Settings.Kafka.stringSerializer, Settings.Kafka.stringSerializer)
+
+    val kafkaService: KafkaService = new KafkaService(kafkaServiceConfig)
+
+    val bitsoBitcoinSource: Source[Message, Promise[Option[Message]]] = Source(List(
+      TextMessage(Settings.Bitso.subscriptionTradesBtcMxn),
+      TextMessage(Settings.Bitso.subscriptionOrdersBtcMxn),
+      TextMessage(Settings.Bitso.subscriptionDiffOrdersBtcMxn)))
+      .concatMat(Source.maybe[Message])(Keep.right)
+
+    val kafkaSink: Sink[Message, Future[Done]] = {
+      Sink.foreach[Message]{
+        case message: TextMessage.Strict => {
+          message.text match {
+            case subscription if subscription contains "subscribe" =>
+              println(subscription)
+            case diffOrder if diffOrder contains "diff-orders" =>
+              kafkaService.publishElement("key", Settings.Kafka.Topics.bitcoinBistoDiffOrders, diffOrder)
+            case order if order contains "orders" =>
+              kafkaService.publishElement("key", Settings.Kafka.Topics.bitcoinBitsoOrders, order)
+            case trade if trade contains "trades" =>
+              kafkaService.publishElement("key", Settings.Kafka.Topics.bitcoinBitsoTrades, trade)
+            case _ => None
+          }
+        }
+        case anything => println(anything)
+      }
+    }
+
+    val webSocketFlow: Flow[Message, Message, Promise[Option[Message]]] =
+      Flow.fromSinkAndSourceMat(kafkaSink, bitsoBitcoinSource)(Keep.right)
 
     val (upgradeResponse, promise) =
-      Http().singleWebSocketRequest(
-        WebSocketRequest(Settings.Bitso.ws),
-        flow)
+      Http().singleWebSocketRequest(WebSocketRequest(Settings.Bitso.ws), webSocketFlow)
 
+    upgradeResponse.onComplete(println)
 
-    //promise.success(None)
   }
-
 }
 
